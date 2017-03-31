68d4178e41b0d06dcbc83ea2996c016e8338c693
driley <dylan.riley@nice.org.uk>
2017-03-31 21:14:01 +0100

    connection string changed to correct db and default connection string deleted

--- GymDiaryCodeFirst/Models/IdentityModels.cs
+++ GymDiaryCodeFirst/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace GymDiaryCodeFirst.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("GymDiaryCF", throwIfV1Schema: false)
         {
         }
 
