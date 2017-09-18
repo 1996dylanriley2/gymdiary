b6c26625c5e84ad289916a5bfa374b78bc25aa80
dylan riley <dylan.riley@hotmail.com>
2017-09-19 00:10:04 +0100

    added pager

---
+++ GymDiaryCodeFirst/Scripts/src/DoWorkout.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+	var pageButton = $('.pagerLink');
+
+	pageButton.click(function () {
+		hideAllVirtualPages();
+
+		var anchor = $(this).find('a');
+		var id = anchor.attr('href');
+		showItem(id);
+
+	});
+
+	function hideAllVirtualPages() {
+		var $vPages = $('.virtual-page');
+		$vPages.each(function () {
+			hideClassIfNotHidden($(this));
+		});
+	}
+
+	function hideClassIfNotHidden(el) {
+		if (!el.hasClass('hide'))
+			el.addClass('hide');
+	}
+
+	function showItem(id) {
+		$(id).removeClass('hide');
+	}
+});
